ceda590f97b0dca9f6779187478ae17872339fb6
alMukaafih <almukaafih@gmail.com>
2023-11-09 08:24:39 +0100

    Renamed Project to qawlun

--- qawlun/asgi.py
+++ qawlun/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tabrik project.
+ASGI config for qawlun project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabrik.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qawlun.settings')
 
 application = get_asgi_application()
